a3d73a2c1c5bc6ec2fed10afbec7bf09697e0079
Administrator <Administrator@Asha.jmandigital.in>
2018-11-12 14:25:11 +0530

    Profile

---
+++ Scripts/Profile/Script1542009980146.groovy
@@ -0,0 +1,29 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+WebUI.callTestCase(findTestCase('Login Page'), [:], FailureHandling.STOP_ON_FAILURE)
+
+'Click on drow-arrow icon on top right corner'
+WebUI.click(findTestObject('Object Repository/Page_Dashboard/i_arrow_drop_down'))
+
+'Click \'Profile\''
+WebUI.click(findTestObject('Object Repository/Page_Dashboard/a_Profile'))
+
+'Click \'Create\''
+WebUI.click(findTestObject('Object Repository/Page_User Profile/i_create'))
+
+'Verify profile update page '
+WebUI.verifyElementPresent(findTestObject('Object Repository/Page_Basic Info/a_Basic Info'), 0)
+
---
+++ Test Suites/Login_Profile.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login_Profile</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>a8602842-2d8f-4a85-87c4-5d5d0fd435ca</testSuiteGuid>
+</TestSuiteEntity>
